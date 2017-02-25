7f4988c147b47c18774eb6fd108aeeded989c1c7
ListenerGao <hnthgys@gmail.com>
2017-02-25 19:31:23 +0800

    再次修改

--- app/src/main/java/com/listenergao/tinkerdemo/MainActivity.java
+++ app/src/main/java/com/listenergao/tinkerdemo/MainActivity.java
@@ -1,14 +1,23 @@
 package com.listenergao.tinkerdemo;
 
+import android.content.Context;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.os.Environment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.tencent.tinker.lib.tinker.Tinker;
 import com.tencent.tinker.lib.tinker.TinkerInstaller;
+import com.tencent.tinker.loader.shareutil.ShareConstants;
+import com.tencent.tinker.loader.shareutil.ShareTinkerInternals;
 
 import java.io.File;
 
@@ -17,6 +26,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button mLoadPath;
     private Button mClearPath;
     private Button mToast;
+    private Button mShowInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,10 +41,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mLoadPath = (Button) findViewById(R.id.btn_load);
         mClearPath = (Button) findViewById(R.id.btn_clear);
         mToast = (Button) findViewById(R.id.btn_toast);
+        mShowInfo = (Button) findViewById(R.id.btn_show);
 
         mLoadPath.setOnClickListener(this);
         mClearPath.setOnClickListener(this);
         mToast.setOnClickListener(this);
+        mShowInfo.setOnClickListener(this);
     }
 
     @Override
@@ -49,7 +61,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
 
             case R.id.btn_toast:
-                Toast.makeText(getApplicationContext(),"使用Tinker修改后的内容",Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(),"原来的内容",Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.btn_show:
+                showInfo(this);
                 break;
         }
     }
@@ -78,5 +94,47 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Toast.makeText(this, "补丁不存在", Toast.LENGTH_SHORT).show();
         }
 
+    }
+
+    public boolean showInfo(Context context) {
+        // add more Build Info
+        final StringBuilder sb = new StringBuilder();
+        Tinker tinker = Tinker.with(getApplicationContext());
+        if (tinker.isTinkerLoaded()) {
+            sb.append(String.format("[patch is loaded] \n"));
+            sb.append(String.format("[buildConfig TINKER_ID] %s \n", "1.7.7"));
+            sb.append(String.format("[buildConfig BASE_TINKER_ID] %s \n", "1.7.7"));
+
+            sb.append(String.format("[buildConfig MESSSAGE] %s \n", "I am the base apk"));
+            sb.append(String.format("[TINKER_ID] %s \n", tinker.getTinkerLoadResultIfPresent().getPackageConfigByName(ShareConstants.TINKER_ID)));
+            sb.append(String.format("[packageConfig patchMessage] %s \n", tinker.getTinkerLoadResultIfPresent().getPackageConfigByName("patchMessage")));
+            sb.append(String.format("[TINKER_ID Rom Space] %d k \n", tinker.getTinkerRomSpace()));
+
+        } else {
+            sb.append(String.format("[patch is not loaded] \n"));
+            sb.append(String.format("[buildConfig TINKER_ID] %s \n", "1.7.7"));
+            sb.append(String.format("[buildConfig BASE_TINKER_ID] %s \n","1.7.7"));
+
+            sb.append(String.format("[buildConfig MESSSAGE] %s \n", "I am the base apk"));
+            sb.append(String.format("[TINKER_ID] %s \n", ShareTinkerInternals.getManifestTinkerID(getApplicationContext())));
+        }
+        sb.append(String.format("[BaseBuildInfo Message] %s \n", ""));
+
+        final TextView v = new TextView(context);
+        v.setText(sb);
+        v.setGravity(Gravity.LEFT | Gravity.CENTER_VERTICAL);
+        v.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 10);
+        v.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        v.setTextColor(0xFF000000);
+        v.setTypeface(Typeface.MONOSPACE);
+        final int padding = 16;
+        v.setPadding(padding, padding, padding, padding);
+
+        final AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setCancelable(true);
+        builder.setView(v);
+        final AlertDialog alert = builder.create();
+        alert.show();
+        return true;
     }
 }
--- app/src/main/java/com/listenergao/tinkerdemo/SimpleTinkerInApplicationLike.java
+++ app/src/main/java/com/listenergao/tinkerdemo/SimpleTinkerInApplicationLike.java
@@ -1,28 +1,31 @@
 package com.listenergao.tinkerdemo;
 
+import android.annotation.TargetApi;
 import android.app.Application;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
+import android.support.multidex.MultiDex;
+import android.util.Log;
 
 import com.tencent.tinker.anno.DefaultLifeCycle;
 import com.tencent.tinker.lib.tinker.TinkerInstaller;
-import com.tencent.tinker.loader.app.ApplicationLike;
+import com.tencent.tinker.loader.app.DefaultApplicationLike;
 import com.tencent.tinker.loader.shareutil.ShareConstants;
 
 /**
  * Created by ListenerGao on 2017/2/18.
- *
  */
 
 /**
  * 注解：@DefaultLifeCycle，其application属性，会在编译期生成一个SimpleTinkerInApplicationLike类
  */
-
-@SuppressWarnings("unused")
 @DefaultLifeCycle(application = ".SimpleTinkerInApplication",
         flags = ShareConstants.TINKER_ENABLE_ALL,
         loadVerifyFlag = false)
-public class SimpleTinkerInApplicationLike extends ApplicationLike {
+public class SimpleTinkerInApplicationLike extends DefaultApplicationLike {
+
+    private static final String TAG = "TinkerInApplicationLike";
 
     public SimpleTinkerInApplicationLike(Application application, int tinkerFlags, boolean tinkerLoadVerifyFlag, long applicationStartElapsedTime, long applicationStartMillisTime, Intent tinkerResultIntent) {
         super(application, tinkerFlags, tinkerLoadVerifyFlag, applicationStartElapsedTime, applicationStartMillisTime, tinkerResultIntent);
@@ -31,12 +34,15 @@ public class SimpleTinkerInApplicationLike extends ApplicationLike {
     @Override
     public void onBaseContextAttached(Context base) {
         super.onBaseContextAttached(base);
-    }
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
+        Log.d(TAG, "SimpleTinkerInApplicationLike的onBaseContextAttached方法执行了");
+        MultiDex.install(base);
         //初始化Tinker
         TinkerInstaller.install(this);
+    }
+
+    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+    public void registerActivityLifecycleCallbacks(Application.ActivityLifecycleCallbacks callback) {
+        getApplication().registerActivityLifecycleCallbacks(callback);
+        Log.d(TAG, "SimpleTinkerInApplicationLike的registerActivityLifecycleCallbacks方法执行了");
     }
 }
